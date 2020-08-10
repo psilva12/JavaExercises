acdbaffb0a30c309bd5db107b103bfc2f001fda2
Paulo Silva <Psilva@qa.com>
2020-08-10 15:56:04 +0100

    Fixed Results

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -7,12 +7,13 @@ public class Results {
     public static int biology = 0;
     public static int total = 0;
     public static double percentage = 0;
+    public static int failed = 0;
 
 
     public static void main(String[] args) {
-        physics = 100;
-        chemistry = 125;
-        biology = 90;
+        physics = 150;
+        chemistry = 150;
+        biology = 150;
         moduleResults();
         passMark();
     }
@@ -22,6 +23,15 @@ public class Results {
         System.out.println("Physics: " + physics);
         System.out.println("Chemistry: " + chemistry);
         System.out.println("Biology: " + biology);
+        if (biology < 60){
+            failed = failed + 1;
+        }
+        if (chemistry < 60){
+            failed = failed + 1;
+        }
+        if (physics < 60){
+            failed = failed + 1;
+        }
         total = physics + chemistry + biology;
         System.out.println("Total: " + total);
     }
@@ -29,6 +39,23 @@ public class Results {
     public static void passMark(){
         percentage = total * 100 / 450;
         System.out.println("Your total percentage is: " + percentage);
-
+        if ( percentage >= 60) {
+            System.out.println();
+            if (percentage == 100){
+                System.out.println("YOU PASSED. YOU GOT PERFECT MARK");
+            }
+            else if (physics >= 60 && biology >= 60 && chemistry >= 60) {
+                System.out.println("YOU PASS, CONGRATULATIONS!");
+            }
+        }
+        else if (failed == 1) {
+            System.out.println("You have failed in one of your modules, therefore you have failed:");
+        }
+        else if (failed == 2) {
+            System.out.println("You have failed in two of your modules, therefore you have failed:");
+        }
+        else {
+            System.out.println("You have failed all of your modules, YOU HAVE FAILED:");
+        }
     }
 }
