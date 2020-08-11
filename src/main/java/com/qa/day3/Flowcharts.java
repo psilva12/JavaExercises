7ae8c647e53cbc568cdfbc2e58529ef834249368
Paulo Silva <Psilva@qa.com>
2020-08-11 11:44:02 +0100

    2 method implemented

--- src/main/java/com/qa/day3/Flowcharts.java
+++ src/main/java/com/qa/day3/Flowcharts.java
@@ -6,12 +6,26 @@ public class Flowcharts {
 
     public static void main(String[] args) {
         method1();
+        method2();
     }
 
     public static void method1(){
 
         for(int a = 10; a < 20; a++){
             System.out.println(a);
+        }
+
+    }
+
+    public static void method2(){
+
+        for(int a = 10; a <= 20; a++){
+            if(a%2 == 0){
+                System.out.println("-");
+            }
+            else{
+                System.out.println("*");
+            }
         }
 
     }
