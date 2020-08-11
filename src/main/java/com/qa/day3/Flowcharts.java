ed90832b866eb9b786f51a6d39fb35bf8d51e2d9
Paulo Silva <Psilva@qa.com>
2020-08-11 11:41:40 +0100

    initial commit

---
+++ src/main/java/com/qa/day3/Flowcharts.java
@@ -0,0 +1,19 @@
+package com.qa.day3;
+
+public class Flowcharts {
+
+
+
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static void method1(){
+
+        for(int a = 10; a < 20; a++){
+            System.out.println(a);
+        }
+
+    }
+
+}
