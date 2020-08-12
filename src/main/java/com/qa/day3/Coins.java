13686a1de4a0852593d512c3373fa0822e6824fa
Paulo Silva <Psilva@qa.com>
2020-08-12 09:31:56 +0100

    Coins is printins all but 1p coins

--- src/main/java/com/qa/day3/Coins.java
+++ src/main/java/com/qa/day3/Coins.java
@@ -2,17 +2,95 @@ package com.qa.day3;
 
 public class Coins {
 
+    public static int twentyPounds = 0;
+    public static int tenPounds = 0;
+    public static int fivePounds = 0;
+    public static int twoPounds = 0;
+    public static int onePound = 0;
+    public static int fiftyP = 0;
+    public static int twentyP = 0;
+    public static int tenP = 0;
+    public static int fiveP = 0;
+    public static int twoP = 0;
+    public static int oneP = 0;
+
 
     public static void main(String[] args) {
         // a = Amount to pay  b = Payed
-        returnAmount(4.52, 20);
+        returnAmount(2.99, 20);
+        System.out.println("Change:");
+        System.out.println("£20: " + twentyPounds);
+        System.out.println("£10: " + tenPounds);
+        System.out.println("£5: " + fivePounds);
+        System.out.println("£2: " + twoPounds);
+        System.out.println("£1: " + onePound);
+        System.out.println("£0.5: " + fiftyP);
+        System.out.println("£0.2: " + twentyP);
+        System.out.println("£0.1: " + tenP);
+        System.out.println("£0.05: " + fiveP);
+        System.out.println("£0.02: " + twoP);
+        System.out.println("£0.01: " + oneP);
     }
 
     public static void returnAmount(double a, double b){
         double change = b;
-
-        while(change != 0){
-
+        change = b - a;
+        while(change >= 0.01){
+            System.out.println(change);
+            if (change - 10 >= 0){
+                tenPounds = tenPounds+ 1;
+                change = change - 10;
+                continue;
+            }
+            if (change - 20 >= 0){
+                twentyPounds = twentyPounds+ 1;
+                change = change - 20;
+                continue;
+            }
+            if (change - 5 >= 0){
+                fivePounds = fivePounds+ 1;
+                change = change - 5;
+                continue;
+            }
+            if (change - 2 >= 0){
+                twoPounds = twoPounds+ 1;
+                change = change - 2;
+                continue;
+            }
+            if (change - 1 >= 0){
+                onePound = onePound+ 1;
+                change = change - 1;
+                continue;
+            }
+            if (change - 0.5 >= 0){
+                fiftyP = fiftyP+ 1;
+                change = change - 0.5;
+                continue;
+            }
+            if (change - 0.2 >= 0){
+                twentyP = twentyP+ 1;
+                change = change - 0.2;
+                continue;
+            }
+            if (change - 0.1 >= 0){
+                tenP = tenP+ 1;
+                change = change - 0.1;
+                continue;
+            }
+            if (change - 0.05 >= 0){
+                fiveP = fiveP+ 1;
+                change = change - 0.05;
+                continue;
+            }
+            if (change - 0.02 >= 0){
+                twoP = twoP+ 1;
+                change = change - 0.02;
+                continue;
+            }
+            if (change - 0.01 >= 0){
+                oneP = oneP+ 1;
+                change = change - 0.01;
+            }
         }
     }
 }
--- src/main/java/com/qa/day3/NumbersRevisited.java
+++ src/main/java/com/qa/day3/NumbersRevisited.java
@@ -7,7 +7,7 @@ public class NumbersRevisited {
 
         printLoop();
     }
-
+    // counts from 1 to 100 writtig the number down
     public static void printLoop() {
 
         for (int i = 1; i <= 100; i++) {
