1bcdcf752ac9305eec53333a4a69e887007c2cc4
Paulo Silva <Psilva@qa.com>
2020-08-12 13:33:30 +0100

    Peer Programming Doggo

---
+++ src/main/java/com/qa/day4/Doggo.java
@@ -0,0 +1,28 @@
+package com.qa.day4;
+public class Doggo {
+    public static void main(String[] args) {
+        method1(12);
+    }
+    public static void method1(int position){
+        int counter = 1;
+        for(int i=1; i<=100; i++) {
+            if (position == i) {
+                continue;
+            } else if(i==11){
+                System.out.println(i+"th");
+            }else if(i==12) {
+                System.out.println(i + "th");
+            }else if(i==13) {
+                System.out.println(i + "th");
+            }else if (i % 10 == 2) {
+                System.out.println(i + "nd");
+            } else if (i % 10 == 3) {
+                System.out.println(i + "rd");
+            } else if(i%10 ==1 ){
+                System.out.println(i +"st");
+            } else {
+                System.out.println(i + "th");
+            }
+        }
+    }
+}
